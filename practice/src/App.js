36e53267ea525bf90274848d8f76e2ba6bdfdb9d
taekyeong2 <kkom.bom1@gmail.com>
2023-05-10 18:10:32 +0900

    life coding

---
+++ practice/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1><a href='/'>web</a></h1>
+      </header>
+      <nav>
+        <li><a href='/read/1'>html</a></li>
+        <li><a href='/read/1'>css</a></li>
+        <li><a href='/read/1'>js</a></li>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
